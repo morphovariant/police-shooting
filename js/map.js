bb43baf27d45afaf067fb0db25898c44478037fe
Jen Yoakum <yoakumjm@uw.edu>
2015-07-04 17:08:52 -0700

    initial

--- js/map.js
+++ js/map.js
@@ -1,32 +1,55 @@
 // Function to draw your map
-var drawMap() = function() {
+var drawMap = function() {
 
-  // Create map and set viewd
+    // Create map and set viewd
+
+    L.mapbox.accessToken = 'pk.eyJ1IjoibW9ycGhvdmFyaWFudCIsImEiOiIzNTZhYTIxZjE3YzJiYjQ5Y2Y0Mzc1ZjJlZTliMmY0NyJ9.gg22GoEx5mShVKjZR37RbA';
+    var map = L.map('container').setView([47.6624,-122.3189],14);
+
+    // Create an tile layer variable using the appropriate url
+
+    var layer = L.tileLayer('http://{s}.tiles.mapbox.com/v4/morphovariant.mkh9fi7o/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibW9ycGhvdmFyaWFudCIsImEiOiIzNTZhYTIxZjE3YzJiYjQ5Y2Y0Mzc1ZjJlZTliMmY0NyJ9.gg22GoEx5mShVKjZR37RbA');
+    //var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+
+    // Add the layer to your map
+
+    layer.addTo(map);
+
+    // Execute your function to get data
+
+    getData()
  
-
-  // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
-}
+};
 
 // Function for getting data
+
+var data;
+
 var getData = function() {
 
-  // Execute an AJAX request to get the data in data/response.js
+    // Execute an AJAX request to get the data in data/response.js
 
+    data = $.ajax({
+        url         : 'data/response.json',
+        type        : 'get',
+        dataType    : 'json'
+    }).done(function() {
+        customBuild()
+    });
 
-  // When your request is successful, call your customBuild function
+    // When your request is successful, call your customBuild function
 
-}
+};
 
-// Do something creative with the data here!  
-var customBuild = function() {
+// Do something creative with the data here!
+var customBuild = function () {
+    data.map(function (d) {
+        new circleMarker([d.latitude, d.longitude], 200, {
+            color: 'blue',
+            opacity: '0.5'
+        }).addTo(map)
+    });
 
-  
-}
+};
 
 
